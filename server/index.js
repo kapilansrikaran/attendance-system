91141b13466a8570943d322dd2652de321678ffb
Kapilan Srikaran <kapilansrikaran@gmail.com>
2024-06-08 11:45:54 +0530

    #3, Add comments and updating the server code

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const SECRET_KEY = 'your_secret_key';
 
-// Middleware
+
 app.use(bodyParser.json());
 app.use(cors()); // Enable CORS
 
